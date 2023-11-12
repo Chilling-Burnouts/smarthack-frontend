7bb6d0394b92bce0b9798aefeb5722869f923a67
Rusu Andrei-Cristian <hello@andreirusu.dev>
2023-11-12 03:09:22 +0200

    add logic for tags

--- src/containers/search-company/index.tsx
+++ src/containers/search-company/index.tsx
@@ -36,9 +36,56 @@ export const SearchCompany: React.FC = () => {
     reset,
   } = form;
 
+  const tags = [
+    {
+      name: "IT - New",
+      path: "/new-it",
+    },
+    {
+      name: "Big earners",
+      path: "/big",
+    },
+    {
+      name: "Green",
+      path: "/green",
+    },
+  ] as {
+    name: string;
+    path: string;
+  }[];
+
+  const searchFromTag = async (path: string) => {
+    setCompanies([]);
+    setIsLoading(true);
+    reset();
+
+    try {
+      const response = await axios.get(
+        `${process.env.NEXT_PUBLIC_API_URL}/companies/search${path}`
+      );
+
+      const companies = response.data.result as ICompany[];
+
+      const filteredCompanies = companies.filter(
+        (comp) =>
+          !portfolioState.portfolio.some(
+            (company) => company.company_name === comp.company_name
+          )
+      );
+
+      setCompanies(filteredCompanies);
+    } catch (err) {
+      console.log(err);
+    }
+
+    setIsLoading(false);
+  };
+
   const searchCompanies = async (data: FormValues) => {
     setCompanies([]);
     setIsLoading(true);
+
+    reset();
 
     try {
       const response = await axios.post(
@@ -61,8 +108,6 @@ export const SearchCompany: React.FC = () => {
     } catch (err) {
       console.log(err);
     }
-
-    reset();
 
     setIsLoading(false);
   };
@@ -102,6 +147,24 @@ export const SearchCompany: React.FC = () => {
               placeholder="Enter company name"
               error={errors.name?.message}
             />
+
+            <div className="mt-4">
+              <span className="text-sm font-medium text-white mb-1 block">
+                Quick Search:
+              </span>
+              <div className="flex flex-wrap gap-2 mt-2">
+                {tags.map((tag, index) => (
+                  <Button
+                    key={index}
+                    type="button"
+                    className="px-3 py-1 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300"
+                    onClick={() => searchFromTag(tag.path)}
+                  >
+                    {tag.name}
+                  </Button>
+                ))}
+              </div>
+            </div>
 
             <Button className="mt-4" type="submit" disabled={isLoading}>
               {isLoading ? "Searching..." : "Search"}
